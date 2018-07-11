f6335b9305fc94e680b266516531cc552e1f4a6d
Zirui Jiang <ziruij@berkeley.edu>
2018-07-11 14:26:36 -0700

    interface revision

--- GUI_interface.py
+++ GUI_interface.py
@@ -38,7 +38,7 @@ option3 = ['Inside','Outside','Other']
 option4 = [0,1,2,3,4,5,6,7,8]
 
 top = Tkinter.Tk()
-top.geometry("850x450")
+top.geometry("1000x500")
 varAir = Tkinter.BooleanVar()
 vard3s = Tkinter.BooleanVar()
 varCO2 = Tkinter.BooleanVar()
